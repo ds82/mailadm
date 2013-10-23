2ff5d43b9bdd6443c61159b1361dc0eb3ccaf3b5
Dennis Saenger <mail@dennis.io>
2013-10-23 19:26:28 +0200

    style(service/address)

--- app/scripts/angular/services/address.js
+++ app/scripts/angular/services/address.js
@@ -3,34 +3,34 @@
  * @author Dennis Sänger, 2013
  */
 define([
-	'jquery', 'app', 'config'
+  'jquery', 'app', 'config'
 ], function( $, app, config ) {
-	'use strict';
+  'use strict';
 
-	app.factory('AddressService', [
-		'$resource',
-	function( $resource ) {
-		return $resource( config.host + '/address/:id', { id: '@source' }, {
-			query: {
-				method:'GET',
-				isArray: true
-			}
-		});
-	}]);
+  app.factory('AddressService', [
+    '$resource',
+  function( $resource ) {
+    return $resource( config.host + '/address/:id', { id: '@source' }, {
+      query: {
+        method:'GET',
+        isArray: true
+      }
+    });
+  }]);
 
-	app.factory('AddressServiceLoader', [
-		'$q', 'AddressService',
-	function( $q, service ) {
-		return function() {
+  app.factory('AddressServiceLoader', [
+    '$q', 'AddressService',
+  function( $q, service ) {
+    return function() {
 
-			var wait = $q.defer();
-			service.query(function( data ) {
-				wait.resolve( data );
-			});
+      var wait = $q.defer();
+      service.query(function( data ) {
+        wait.resolve( data );
+      });
 
-			return wait.promise;
-		}
-	}]);
+      return wait.promise;
+    }
+  }]);
 
 });
 
