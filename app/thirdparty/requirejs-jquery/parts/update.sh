904add4b925cd814917d77fedb4258e2a7ddca0f
Dennis Saenger <mail@dennis.io>
2014-06-21 14:00:21 +0200

    removed no longer used thirdparty deps

--- app/thirdparty/requirejs-jquery/parts/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Updates the built require-jquery.js file if either jQuery or RequireJS
-# is updated. The new version of either file should be placed in this
-# directory, then this command should be run.
-
-cat require.js jquery.js > ../jquery-require-sample/webapp/scripts/require-jquery.js
