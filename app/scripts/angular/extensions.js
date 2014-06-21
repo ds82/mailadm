91aa277b3627827ebbfa60cbf2b1ec448d501b06
Dennis Saenger <mail@dennis.io>
2014-06-21 15:58:09 +0200

    remove deprecated extensions

--- app/scripts/angular/extensions.js
+++ app/scripts/angular/extensions.js
@@ -5,6 +5,5 @@ require('thirdparty/angular-cookies/angular-cookies');
 
 require('thirdparty/ng-contextmenu/contextmenu');
 
-require('thirdparty/yom-nganimate/animate');
 require('thirdparty/angular-translate/angular-translate');
 require('thirdparty/angular-bootstrap-switch/angular-bootstrap-switch');
