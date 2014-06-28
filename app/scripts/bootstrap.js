11e74e2161d4012699a29e1693a32b241f6763b3
Dennis Saenger <mail@dennis.io>
2014-06-28 22:42:11 +0200

    add bootstrap dropdown

--- app/scripts/bootstrap.js
+++ app/scripts/bootstrap.js
@@ -1,4 +1,6 @@
 require('thirdparty/bootstrap/js/modal');
 require('thirdparty/bootstrap/js/tooltip');
 
+require('thirdparty/bootstrap/js/dropdown');
+
 require('thirdparty/bootstrap-switch/js/bootstrap-switch');
