4cb5a8eada3cca190de308414a99677a362a99c4
Dennis Sänger <mail@dennis.io>
2015-03-13 14:16:28 +0100

    chore(gulp): add default task

--- gulp/index.js
+++ gulp/index.js
@@ -2,9 +2,16 @@
 
 var fs   = require('fs');
 var path = require('path');
+var gulp = require('gulp');
 
 var files = fs.readdirSync(path.join(__dirname, 'tasks'));
 
 files
   .filter(function(file) { return !!file.match(/\.js$/); })
   .forEach(function(file) { require('./tasks/' + file); });
+
+gulp.task('default', [
+  'browserify:common',
+  'browserify:bundle',
+  'less'
+]);
