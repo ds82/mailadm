be837f1430bddf0baf8456c8e789a7603add7290
Dennis Sänger <mail@dennis.io>
2015-06-02 21:18:55 +0200

    chore(angular): integrate angular-new-router v0.5.3

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -1,12 +1,3 @@
 'use strict';
 
-angular.module('app', [])
-  .constant('APP_NAME', 'boilerplate')
-  .controller('AppCtrl', AppCtrl);
-
-AppCtrl.$inject = ['APP_NAME'];
-function AppCtrl(APP_NAME) {
-  var vm = this;
-
-  vm.APP_NAME = APP_NAME;
-}
+require('./app');
