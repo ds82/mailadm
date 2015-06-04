10f1c35801df07ffd09dbdabf07cf57224cb27f9
Dennis Sänger <mail@dennis.io>
2015-06-04 22:15:46 +0200

    feat(dashboard): draft of dashboard module

---
+++ app/modules/dashboard/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('mailadm.dashboard', [
+  'ngNewRouter'
+]).controller('mailadm.dashboard.mainCtrl', Main);
+
+Main.$inject = [];
+
+function Main() {
+  // var vm = this;
+}
